dd3db549f0cb9f44630af7cc019a1f83bcd05f36
Lucas J D Oliveira <ljoliveira99@outlook.com>
2024-10-17 14:22:57 -0300

    criando interceptor

---
+++ src/app/common/helpers/interceptors/authenticateInterceptor.ts
@@ -0,0 +1,32 @@
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { TokenService } from "../../../services/token.service";
+import { environment } from "../../../../environments/environment.development";
+
+@Injectable()
+export class AuthenticateInterceptor implements HttpInterceptor {
+
+  constructor(private tokenService: TokenService) {
+
+  }
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    if (req.url.split('/')[2] == environment.api_url.split('/')[2]) {
+      return next.handle(req);
+    }
+
+    const token = this.tokenService.getToken();
+    if (token) {
+
+      req = req.clone({
+        setHeaders: {
+          Authorization: `Bearer ${token}`,
+          token: `${token}`
+        }
+      });
+
+    }
+    return next.handle(req);
+  }
+}
+
