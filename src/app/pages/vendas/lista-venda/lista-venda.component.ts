5cc405749c991e87ebd326e4b59388264449893b
Lucas J D Oliveira <ljoliveira99@outlook.com>
2024-12-14 01:10:49 -0300

    implementando tela de relatorio de vendas

--- src/app/pages/vendas/lista-venda/lista-venda.component.ts
+++ src/app/pages/vendas/lista-venda/lista-venda.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import Column from '../../../models/custonsModels/CustomTable/CustomColumn';
+import { VendaService } from '../../../services/Venda.service';
+import { MessageService } from 'primeng/api';
+import { Venda } from '../../../models/entityModels/Venda/Venda';
 
 @Component({
   selector: 'app-lista-venda',
@@ -6,5 +10,60 @@ import { Component } from '@angular/core';
   styleUrl: './lista-venda.component.scss'
 })
 export class ListaVendaComponent {
+
+  vendas: any[] = [];
+  loading: boolean = false;
+
+  constructor(private service: VendaService, private messageSerivce: MessageService) {
+
+  }
+
+
+  showMessage(type: string, title: string, message: string) {
+    this.messageSerivce.add({
+      severity: type,
+      summary: title,
+      detail: message,
+      key: 'trlrv',
+      life: 3000
+    })
+  }
+
+  getColumnList(): Column[] {
+    return [
+      new Column("cliente", "Cliente"),
+      new Column("data", "Data"),
+      new Column("total", "Total"),
+    ]
+  }
+
+  getVendas() {
+    this.service.listAll().subscribe({
+      next: (venda: Venda[]) => {
+        this.vendas = venda;
+        this.loading = false;
+      }, error: () => {
+        this.loading = false;
+      }
+    })
+  }
+
+  deletar(id: string) {
+    this.service.delete(id).subscribe({
+      next: (response: boolean) => {
+        if (response) {
+          this.loading = false;
+          this.showMessage('success', 'Produtos', 'Produto deletado com sucesso!');
+          this.getVendas();
+        }
+      }, error: (err: any) => {
+
+      }
+    })
+  }
+
+  ngOnInit() {
+    this.getVendas();
+  }
 
 }
