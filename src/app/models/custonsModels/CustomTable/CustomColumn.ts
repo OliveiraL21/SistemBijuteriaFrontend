693e68e568615c668608f0022656ac47a3894f95
OliveiraL21 <lucasoliveira508@gmail.com>
2025-09-05 16:29:06 -0300

    criando classes dinamicas em table custom componente

--- src/app/models/custonsModels/CustomTable/CustomColumn.ts
+++ src/app/models/custonsModels/CustomTable/CustomColumn.ts
@@ -2,11 +2,13 @@ export default class Column {
   field: string;
   header: string;
   filter: boolean;
+  classes: string[];
 
 
-  constructor(field: string, header: string, filter: boolean = false) {
+  constructor(field: string, header: string, filter: boolean = false, classes: string[] = []) {
     this.field = field;
     this.header = header;
     this.filter = filter;
+    this.classes = classes;
   }
 }
