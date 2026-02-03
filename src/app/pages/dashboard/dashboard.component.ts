a902f955fc843c1462591c17dcef777d2a58ba7a
OliveiraL21 <lucasoliveira508@gmail.com>
2026-02-03 13:03:02 -0300

    ajustes dashboard

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { MaletaService } from '../../services/Maleta.service';
+import { Maleta } from '../../models/entityModels/maleta/Maleta';
 
 @Component({
   selector: 'app-dashboard',
@@ -9,14 +10,17 @@ import { MaletaService } from '../../services/Maleta.service';
 })
 export class DashboardComponent {
   form!: FormGroup;
-
+  maleta: Maleta = new Maleta();
   constructor(private fb: FormBuilder, private maletaSeervice: MaletaService) {
 
   }
 
   getMaletaAtual() {
     this.maletaSeervice.getMaletaAtual().subscribe(maleta => {
-      console.log(maleta);
+      this.maleta = maleta;
+      this.maleta.Envio = new Date(maleta.envio.split('/')[2], maleta.envio.split('/')[1] - 1, maleta.envio.split('/')[0]);
+      this.maleta.Pagamento = new Date(maleta.pagamento.split('/')[2], maleta.pagamento.split('/')[1] - 1, maleta.pagamento.split('/')[0]);
+      this.maleta.Troca = new Date(maleta.troca.split('/')[2], maleta.troca.split('/')[1] - 1, maleta.troca.split('/')[0]);
     });
   }
 
