cc7759f9591de2b0ae785d82978ce60561bd477b
OliveiraL21 <lucasoliveira508@gmail.com>
2026-02-14 12:50:45 -0300

    codificando atualizações automaticas

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
 // main.js
 
 // Modules to control application life and create native browser window
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, autoUpdater, dialog } = require("electron");
+const { autoUpdater: updater } = require('electron-updater');
 const path = require("path");
 app.commandLine.appendSwitch('high-dpi-support', 'true');
 app.commandLine.appendSwitch('force-device-scale-factor', '1');
+
+updater.logger = require('electron-log');
+updater.logger.transports.file.level = 'info';
+
 
 const createWindow = () => {
   // Create the browser window.
@@ -37,7 +42,23 @@ app.whenReady().then(() => {
     // dock icon is clicked and there are no other windows open.
     if (BrowserWindow.getAllWindows().length === 0) createWindow();
   });
+
+  updater.checkForUpdatesAndNotify();
 });
+
+updater.on('update-downloaded', (info) => {
+  dialog.showMessageBox({
+    type: 'info',
+    title: 'Atualização Disponível',
+    message: 'Uma nova versão foi baixada. Deseja instalar agora?',
+    buttons: ['Sim', 'Depois']
+  }).then(result => {
+    if (result.response === 0) {
+      updater.quitAndInstall();
+    }
+  })
+});
+
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
