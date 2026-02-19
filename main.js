32d1f7d9f9a1365ae8f245dbf2bd25feefa24c19
OliveiraL21 <lucasoliveira508@gmail.com>
2026-02-18 21:57:11 -0300

    tentando ajustar icone de desktop da aplicação

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ const createWindow = () => {
   const mainWindow = new BrowserWindow({
     width: 1200,
     height: 800,
-    icon: path.join(__dirname, 'dist/sistem-bijuteria-frontend/browser/assets/images/diamond-ring.ico'),
+    icon: app.platform === 'linux' ? path.join(__dirname, 'dist/sistem-bijuteria-frontend/browser/assets/images/diamond-ring.png') : path.join(__dirname, 'dist/sistem-bijuteria-frontend/browser/assets/images/diamond-ring.ico'),
     autoHideMenuBar: true,
     webPreferences: {
       preload: path.join(__dirname, "dist/sistem-bijuteria-frontend/browser/preload.js"),
